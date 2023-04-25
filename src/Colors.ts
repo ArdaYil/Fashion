c523d4ac50b97215ab8441468d383a20bdab29ab
Arda Yildirim <ardayildirim05@icloud.com>
2023-04-25 22:23:53 +0200

    Define the nike products

---
+++ src/Colors.ts
@@ -0,0 +1,12 @@
+enum Colors {
+  RED,
+  YELLOW,
+  BLUE,
+  WHITE,
+  GRAY,
+  GREEN,
+  CYAN,
+  BEIGE,
+}
+
+export default Colors;
---
+++ src/products.ts
@@ -0,0 +1,33 @@
+import Colors from "./Colors";
+
+export default {
+  nike: {
+    "Nike Jordans 1 High": {
+      title: "Nike Jordans 1 High",
+      price: 420,
+      colors: [Colors.RED, Colors.YELLOW, Colors.BLUE],
+    },
+
+    "Nike Airforce 1": {
+      title: "Nike Airforce 1",
+      price: 120,
+      colors: [Colors.WHITE, Colors.GRAY],
+    },
+
+    "Nike Air Max": {
+      title: "Nike Airforce 1",
+      price: 220,
+      colors: [Colors.RED, Colors.GREEN, Colors.BLUE],
+    },
+
+    "Nike Mercurial Vaper": {
+      title: "Nike Airforce 1",
+      price: 320,
+      colors: [Colors.CYAN, Colors.BEIGE],
+    },
+  },
+  adidas: {},
+  converse: {},
+
+  "new balance": {},
+};
