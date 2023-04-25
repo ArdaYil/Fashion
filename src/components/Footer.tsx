949fd18e59e33d19ed2e5cbdd2a33fec793ab388
Arda Yildirim <ardayildirim05@icloud.com>
2023-04-25 21:56:46 +0200

    Implement mobile styling for homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,47 @@
+import { Link } from "react-router-dom";
+import Title from "./Title";
+
 export default function Footer() {
   return (
     <footer className="footer">
       <section className="footer__section">
-        <p className="footer__section__text">Contact Us</p>
-        <p className="footer__section__text">About Us</p>
+        <Link to="/" className="footer__section__text">
+          Contact Us
+        </Link>
+        <Link to="/" className="footer__section__text">
+          About Us
+        </Link>
       </section>
       <section className="footer__section">
-        <p className="footer__section__text">FAQ</p>
-        <p className="footer__section__text">Returns & Exchanges</p>
+        <Link to="/" className="footer__section__text">
+          FAQ
+        </Link>
+        <Link to="/" className="footer__section__text">
+          Returns & Exchanges
+        </Link>
       </section>
       <section className="footer__section">
-        <p className="footer__section__text">Terms of Service</p>
-        <p className="footer__section__text">Privacy Policy</p>
-        <p className="footer__section__text">GDPR</p>
+        <Link to="/" className="footer__section__text">
+          Terms of Service
+        </Link>
+        <Link to="/" className="footer__section__text">
+          Privacy Policy
+        </Link>
+        <Link to="/" className="footer__section__text">
+          GDPR
+        </Link>
       </section>
-      <section className="footer__section">
-        <img src="../../public/icons/discord.png" />
-        <img src="../../public/icons/twitter.png" />
-        <img src="../../public/icons/facebook.png" />
-        <img src="../../public/icons/instagram.png" />
+      <section className="footer__section footer__section-icon-container">
+        <img className="footer__icon" src="../../public/icons/discord.png" />
+        <img className="footer__icon" src="../../public/icons/twitter.png" />
+        <img className="footer__icon" src="../../public/icons/facebook.png" />
+        <img className="footer__icon" src="../../public/icons/instagram.png" />
       </section>
-      <section className="footer__section">
-        <h2>Fashion</h2>
-        <p className="footer__section__text"></p>
+      <section className="footer__section footer__title-section">
+        <Title className="nav-bar__title footer__title" />
+        <Link to="/" className="footer__section__text">
+          &copy; 2023 Fashion. All Rights Reserved
+        </Link>
       </section>
     </footer>
   );
--- src/components/hompage/ProductBrand.tsx
+++ src/components/hompage/ProductBrand.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function ProductBrand({ src, brand, alt }: Props) {
   return (
     <article className="product-brands__brand">
-      <h1 className="product-brands__heading">{`New ${brand} Products`}</h1>
+      <h1 className="product-brands__brand__heading">{`New ${brand} Products`}</h1>
       <img src={src} alt={alt} className="product-brands__brand__image" />
       <Link to="/" className="btn--orange product-brands__brand__buy">
         Buy Now!
